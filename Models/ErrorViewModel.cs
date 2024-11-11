51df0986fbfc28e81114529eff84f5758b9f72ff
Cristina-Colcha <criscolcha@gmail.com>
2024-11-10 21:28:40 -0500

    style: :tada: cambio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SumarNumerosApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
